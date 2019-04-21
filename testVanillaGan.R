c28a2851801705c61576e7013276c711074574c3
Nick Tustison <ntustison@gmail.com>
2019-04-20 20:21:56 -0700

    ENH:  vanilla GAN

---
+++ testVanillaGan.R
@@ -0,0 +1,27 @@
+library( keras )
+library( ANTsRNet )
+
+keras::backend()$clear_session()
+
+# Let's use the mnist data set.
+
+mnist <- dataset_mnist()
+
+numberOfTrainingData <- length( mnist$train$y )
+
+inputImageSize <- c( dim( mnist$train$x[1,,] ), 1 )
+
+x <- array( data = mnist$train$x / 255, dim = c( numberOfTrainingData, inputImageSize ) )
+y <- mnist$train$y
+
+numberOfClusters <- length( unique( mnist$train$y ) )
+
+# Instantiate the DCEC model
+
+ganModel <- VanillaGanModel$new(
+   inputImageSize = inputImageSize,
+   latentDimension = 100 )
+
+ganModel$train( x, numberOfEpochs = 100 )
+
+
