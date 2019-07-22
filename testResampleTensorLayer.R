84bcd97960d81d2716a5801a0cce05dea607766c
Nick Tustison <ntustison@gmail.com>
2019-07-22 10:33:06 -0700

    ENH:  3D example.

--- testResampleTensorLayer.R
+++ testResampleTensorLayer.R
@@ -4,6 +4,12 @@ library( keras )
 library( magick )
 library( tensorflow )
 
+
+####
+#
+#  Test 2-D
+#
+
 bikeFile <- tempfile( pattern = "bike", fileext = ".jpg" )
 download.file(
   url = "https://user-images.githubusercontent.com/22609465/36634042-4168652a-1964-11e8-90a9-2c480b97eff7.jpg",
@@ -16,7 +22,7 @@ f <- array( data = f, dim = c( 1, dim( f ) ) )
 
 ft <- layer_input( batch_shape = dim( f ) )
 
-shape = c( 700, 700 )
+shape <- c( 700, 700 )
 
 types <- c( 'nearestNeighbor', 'linear', 'cubic' )
 for( i in seq_len( length( types ) ) )
@@ -32,4 +38,34 @@ for( i in seq_len( length( types ) ) )
   image_write( img, path =
     paste0( "out_", types[i], format = ".jpg" ) )
   }
+
+####
+#
+# Test 3-D
+#
+
+brainFile <- getANTsRData( "ch2b" )
+
+f <- antsImageRead( brainFile )
+f <- as.array( f )
+f <- aperm( f, c( 3, 2, 1 ) )
+f <- array( data = f, dim = c( 1, dim( f ), 1 ) )
+
+ft <- layer_input( batch_shape = dim( f ) )
+
+shape <- c( 200, 200, 200 )
+
+types <- c( 'nearestNeighbor', 'linear', 'cubic' )
+for( i in seq_len( length( types ) ) )
+  {
+  output <- layer_resample_tensor_3d( ft, shape, types[i] )
+  model <- keras_model( inputs = ft, outputs = output )
+  outputArray <- drop( model %>% predict( f ) )
+
+  # we skip calculating the correct header info as it's not
+  # important for demonstrating functionality.
+  outputImage <- as.antsImage( outputArray )
+  antsImageWrite( outputImage, paste0( "out3D_", types[i], ".nii.gz" ) )
+  }
+
 
