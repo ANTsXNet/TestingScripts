7adaf7e8f8fb20445adf432453ec287b7749ecbc
Nick Tustison <ntustison@gmail.com>
2019-08-13 11:17:23 -0700

    ENH:  Better example.

--- testVanillaGan.R
+++ testVanillaGan.R
@@ -11,17 +11,19 @@ numberOfTrainingData <- length( mnist$train$y )
 
 inputImageSize <- c( dim( mnist$train$x[1,,] ), 1 )
 
-x <- array( data = mnist$train$x / 255, dim = c( numberOfTrainingData, inputImageSize ) )
+
+x <- array( data = ( mnist$train$x / 127.5 - 1 ),
+            dim = c( numberOfTrainingData, inputImageSize ) )
 y <- mnist$train$y
 
 numberOfClusters <- length( unique( mnist$train$y ) )
 
-# Instantiate the DCEC model
+# Instantiate and train the GAN model
 
 ganModel <- VanillaGanModel$new(
    inputImageSize = inputImageSize,
    latentDimension = 100 )
 
-ganModel$train( x, numberOfEpochs = 100 )
-
+ganModel$train( x, numberOfEpochs = 30000, sampleInterval = 100,
+  sampleFilePrefix = "/Users/ntustison/Desktop/SampleImages/sample" )
 
