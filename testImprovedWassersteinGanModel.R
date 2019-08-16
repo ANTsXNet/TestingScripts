3f7d28805d4e61966d8967784e529298f8cae1cf
Nick Tustison <ntustison@gmail.com>
2019-08-16 11:02:05 -0700

    ENH:  Add Improved WGAN

---
+++ testImprovedWassersteinGanModel.R
@@ -0,0 +1,30 @@
+library( keras )
+library( ANTsRNet )
+
+keras::backend()$clear_session()
+
+# Let's use the mnist data set.
+
+mnist <- dataset_mnist()
+
+numberOfTrainingData <- length( mnist$train$y )
+
+inputImageSize <- c( dim( mnist$train$x[1,,] ), 1 )
+
+
+x <- array( data = ( mnist$train$x / 127.5 - 1 ),
+            dim = c( numberOfTrainingData, inputImageSize ) )
+y <- mnist$train$y
+
+numberOfClusters <- length( unique( mnist$train$y ) )
+
+# Instantiate and train the GAN model
+
+ganModel <- ImprovedWassersteinGanModel$new(
+   inputImageSize = inputImageSize,
+   latentDimension = 100 )
+
+ganModel$train( x, numberOfEpochs = 4000, batchSize = 32,
+  sampleInterval = 100,
+  sampleFilePrefix = "./WGanGpSampleImages/sample" )
+
