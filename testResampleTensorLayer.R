26fc0069666cea8c421f3eac22b755c7004b98ae
Nick Tustison <ntustison@gmail.com>
2020-12-17 15:55:47 -0800

    ENH:  Add brian's variant.

--- testResampleTensorLayer.R
+++ testResampleTensorLayer.R
@@ -15,69 +15,64 @@ download.file(
   url = "https://user-images.githubusercontent.com/22609465/36634042-4168652a-1964-11e8-90a9-2c480b97eff7.jpg",
   destfile = bikeFile )
 
-# f <- antsImageRead( bikeFile )
-# f <- as.array( f )
-# f <- aperm( f, c( 3, 2, 1 ) )
+f <- antsImageRead( bikeFile )
+f <- as.array( f )
+f <- aperm( f, c( 3, 2, 1 ) )
 
-# # ft <- layer_input( shape = dim( f ) )
-# ft <- layer_input( shape = list( NULL, NULL, 3 ) )
+# ft <- layer_input( shape = dim( f ) )
+ft <- layer_input( shape = list( NULL, NULL, 3 ) )
 
-# targetShape <- c( 700L, 700L )
+targetShape <- c( 700L, 700L )
 
-# types <- c( 'nearestNeighbor', 'linear', 'cubic' )
-# for( i in seq_len( length( types ) ) )
-#   {
-#   cat( "Doing 2-D", types[i], "\n" )
-#   output <- layer_resample_tensor_2d( ft, targetShape, types[i] )
-#   model <- keras_model( inputs = ft, outputs = output )
-#   batchX <- array( data = f, dim = c( 1, dim( f ) ) )
-#   outputImage <- drop( model %>% predict( batchX ) )
+types <- c( 'nearestNeighbor', 'linear', 'cubic' )
+for( i in seq_len( length( types ) ) )
+  {
+  cat( "Doing 2-D", types[i], "\n" )
+  output <- layer_resample_tensor_2d( ft, targetShape, types[i] )
+  model <- keras_model( inputs = ft, outputs = output )
+  batchX <- array( data = f, dim = c( 1, dim( f ) ) )
+  outputImage <- drop( model %>% predict( batchX ) )
 
-#   # Rescale from [0, 1]
-#   ys <- ( outputImage - min( outputImage ) ) /
-#         ( max( outputImage ) - min( outputImage ) )
-#   img <- magick::image_read( ys )
-#   image_write( img, path =
-#     paste0( "out_", types[i], format = ".jpg" ) )
-#   }
+  # Rescale from [0, 1]
+  ys <- ( outputImage - min( outputImage ) ) /
+        ( max( outputImage ) - min( outputImage ) )
+  img <- magick::image_read( ys )
+  image_write( img, path =
+    paste0( "out_", types[i], format = ".jpg" ) )
+  }
 
 ####
 #
 # Brian's variant
 #
 
-targetShape <- c( 700L, 700L )
+targetShape <- c( 600L, 700L )
 
-source <- layer_input( shape = list( NULL, NULL, 1 ) )
+source <- layer_input( shape = list( NULL, NULL, 3 ) )
 target <- layer_input( shape = list( NULL, NULL, 1 ) )
 
-sourceImage <- splitChannels( antsImageRead( bikeFile ) )[[1]]
-targetImage <- as.antsImage( array( data = 0, dim = c( targetShape ) ) )
+sourceImage <- antsImageRead( bikeFile )
+targetImage <- as.antsImage( array( data = 0, dim = targetShape ) )
 
-sourceArray <- array( data = as.array( sourceImage ), dim = c( 1, dim( sourceImage ), 1 ) )
+sourceArray <- aperm( as.array( sourceImage ), c( 3, 2, 1 ) )
+sourceArray <- array( data = sourceArray, dim = c( 1, dim( sourceArray ) ) )
 targetArray <- array( data = as.array( targetImage ), dim = c( 1, dim( targetImage ), 1 ) )
 
 types <- c( 'nearestNeighbor', 'linear', 'cubic' )
 for( i in seq_len( length( types ) ) )
   {
   cat( "Doing Brian's 2-D", types[i], "\n" )
-  output <- source %>% layer_resample_tensor_to_target_tensor_2d( target, types[i] )
-
-  cat( "Build model.\n" )
+  output <- list( source, target ) %>% layer_resample_tensor_to_target_tensor_2d( types[i] )
   model <- keras_model( inputs = list( source, target ), outputs = output )
+  outputImage <- drop( model %>% predict( list( sourceArray, targetArray ) ) )
 
-
-  # batchX <- array( data = f, dim = c( 1, dim( f ) ) )
-  # outputImage <- drop( model %>% predict( list( batchX ) )
-
-  # # Rescale from [0, 1]
-  # ys <- ( outputImage - min( outputImage ) ) /
-  #       ( max( outputImage ) - min( outputImage ) )
-  # img <- magick::image_read( ys )
-  # image_write( img, path =
-  #   paste0( "out_brian_", types[i], format = ".jpg" ) )
+  # Rescale from [0, 1]
+  ys <- ( outputImage - min( outputImage ) ) /
+        ( max( outputImage ) - min( outputImage ) )
+  img <- magick::image_read( ys )
+  image_write( img, path =
+    paste0( "out_brian_", types[i], format = ".jpg" ) )
   }
-
 
 
 ####
@@ -96,20 +91,46 @@ ft <- layer_input( shape = list( NULL, NULL, NULL, 1 ) )
 
 shape <- c( 100L, 110L, 120L )
 
+# types <- c( 'nearestNeighbor', 'linear', 'cubic' )
+# for( i in seq_len( length( types ) ) )
+#   {
+#   cat( "Doing 3-D", types[i], "\n" )
+#   output <- layer_resample_tensor_3d( ft, shape, types[i] )
+#   model <- keras_model( inputs = ft, outputs = output )
+#   batchX <- array( data = f, dim = c( 1, dim( f ) ) )
+#   outputArray <- model %>% predict( batchX, verbose = TRUE )
+
+#   # we skip calculating the correct header info as it's not
+#   # important for demonstrating functionality.
+#   outputImage <- as.antsImage( drop( outputArray ) )
+#   antsImageWrite( outputImage, paste0( "out3D_", types[i], ".nii.gz" ) )
+#   }
+
+
+####
+#
+# Brian's variant
+#
+
+targetShape <- c( 100L, 110L, 120L )
+
+source <- layer_input( shape = list( NULL, NULL, NULL, 1 ) )
+target <- layer_input( shape = list( NULL, NULL, NULL, 1 ) )
+
+sourceArray <- array( data = f, dim = c( 1, dim( f ) ) )
+targetArray <- array( data = 0, dim = c( 1, targetShape, 1 ) )
+
 types <- c( 'nearestNeighbor', 'linear', 'cubic' )
 for( i in seq_len( length( types ) ) )
   {
-  cat( "Doing 3-D", types[i], "\n" )
-  output <- layer_resample_tensor_3d( ft, shape, types[i] )
-  model <- keras_model( inputs = ft, outputs = output )
-  batchX <- array( data = f, dim = c( 1, dim( f ) ) )
-  outputArray <- model %>% predict( batchX, verbose = TRUE )
+  cat( "Doing Brian's 3-D", types[i], "\n" )
+  output <- list( source, target ) %>% layer_resample_tensor_to_target_tensor_3d( types[i] )
+  model <- keras_model( inputs = list( source, target ), outputs = output )
+  outputArray <- model %>% predict( list( sourceArray, targetArray ) )
 
   # we skip calculating the correct header info as it's not
   # important for demonstrating functionality.
   outputImage <- as.antsImage( drop( outputArray ) )
-  antsImageWrite( outputImage, paste0( "out3D_", types[i], ".nii.gz" ) )
+  antsImageWrite( outputImage, paste0( "out3D_brian_", types[i], ".nii.gz" ) )
   }
-
-
 
